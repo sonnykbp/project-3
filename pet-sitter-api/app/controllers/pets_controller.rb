7f69b2ee375121475ee835cc5071adcb72244117
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-06-26 09:51:55 -0400

    updated api

--- pet-sitter-api/app/controllers/pets_controller.rb
+++ pet-sitter-api/app/controllers/pets_controller.rb
@@ -1,7 +1,18 @@
 class PetsController < ApplicationController
   def index
+    @owner = Owner.find(params[:owner_id])
+    @pet = @owner.pets
+
+
+    render json: @pet
   end
 
   def show
+    @owner = Owner.find(params[:owner_id])
+    @pet = Pet.find(params[:id])
+
+    render json: @pet
+
   end
+
 end
--- pet-sitter-api/app/controllers/sitters_controller.rb
+++ pet-sitter-api/app/controllers/sitters_controller.rb
@@ -1,7 +1,12 @@
 class SittersController < ApplicationController
   def index
+    @sitters = Sitter.all
+    render json: @sitters
   end
 
   def show
+    @sitter = Sitter.find(params[:id])
+
+    render json: @sitter
   end
 end
